8926d496be9196694305de1bd8af7e0dc67d7a8b
Intoloshubham <shubham.sahu@intoloindia.com>
2022-11-11 15:12:21 +0530

    11 11 2022 comit 3rd

--- lib/view/residential.dart
+++ lib/view/residential.dart
@@ -95,7 +95,7 @@ class _ResidentialState extends State<Residential> {
                       //     await SharedPreferences.getInstance();
                       // var flat_house_const =
                       //     prefs.setInt('projectHouseTypeId', FLAT_HOUSE);
-                          setProjectTypeIdHouse(FLAT_HOUSE);
+                      setProjectTypeIdHouse(FLAT_HOUSE);
                       // var getProjectTypeId = await provider.getProjectType();
                     },
                     child: Stack(
@@ -237,47 +237,47 @@ class _ResidentialState extends State<Residential> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: iconColor,
-              ),
-              label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.add_circle_outline,
-                color: iconColor,
-              ),
-              label: ""),
-          BottomNavigationBarItem(
-              icon: IconButton(
-                icon: Icon(
-                  Icons.person,
-                  color: iconColor,
-                ),
-                onPressed: () {
-                  isLogged
-                      // ? const Profile()
-                      ? Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Profile(),
-                          ))
-                      : showDialog(
-                          builder: (context) => loginDialog(context),
-                          context: (context));
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //         icon: Icon(
+      //           Icons.home,
+      //           color: iconColor,
+      //         ),
+      //         label: ""),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(
+      //           Icons.add_circle_outline,
+      //           color: iconColor,
+      //         ),
+      //         label: ""),
+      //     BottomNavigationBarItem(
+      //         icon: IconButton(
+      //           icon: Icon(
+      //             Icons.person,
+      //             color: iconColor,
+      //           ),
+      //           onPressed: () {
+      //             isLogged
+      //                 // ? const Profile()
+      //                 ? Navigator.push(
+      //                     context,
+      //                     MaterialPageRoute(
+      //                       builder: (context) => Profile(),
+      //                     ))
+      //                 : showDialog(
+      //                     builder: (context) => loginDialog(context),
+      //                     context: (context));
 
-                  // showDialog(
-                  //   builder: (context) => loginDialog(context),
-                  //   context: context,
-                  // );
-                },
-              ),
-              label: ""),
-        ],
-      ),
+      //             // showDialog(
+      //             //   builder: (context) => loginDialog(context),
+      //             //   context: context,
+      //             // );
+      //           },
+      //         ),
+      //         label: ""),
+      //   ],
+      // ),
     );
   }
 }
